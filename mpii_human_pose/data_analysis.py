1781e7402cb2443702a3b62ab1cecab230bd39f3
Nicole Hoelzl <hoelzl.nici@gmail.com>
2020-12-30 17:34:09 +0100

    Reformat code

--- mpii_human_pose/data_analysis.py
+++ mpii_human_pose/data_analysis.py
@@ -5,12 +5,11 @@ Created on 30.12.2020, by Nicole Hölzl
 # import packages
 import os
 import sys
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt # plotting
-import numpy as np # linear algebra
-import os # accessing directory structure
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt  # plotting
+import numpy as np  # linear algebra
+import os  # accessing directory structure
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 
 # distribution graphs (histogram/bar graph) of column data
@@ -21,7 +20,7 @@ def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
     nRow, nCol = df.shape
     columnNames = list(df)
     nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
-    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
+    plt.figure(num=None, figsize=(6 * nGraphPerRow, 8 * nGraphRow), dpi=80, facecolor='w', edgecolor='k')
     for i in range(min(nCol, nGraphShown)):
         plt.subplot(nGraphRow, nGraphPerRow, i + 1)
         columnDf = df.iloc[:, i]
@@ -31,23 +30,23 @@ def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
         else:
             columnDf.hist()
         plt.ylabel('counts')
-        plt.xticks(rotation = 90)
+        plt.xticks(rotation=90)
         plt.title(f'{columnNames[i]} (column {i})')
-    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
+    plt.tight_layout(pad=1.0, w_pad=1.0, h_pad=1.0)
     plt.show()
 
 
 # correlation matrix
 def plotCorrelationMatrix(df, graphWidth):
     filename = df.dataframeName
-    df = df.dropna('columns') # drop columns with NaN
-    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    df = df.dropna('columns')  # drop columns with NaN
+    df = df[[col for col in df if df[col].nunique() > 1]]  # keep columns where there are more than 1 unique values
     if df.shape[1] < 2:
         print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
         return
     corr = df.corr()
     plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
-    corrMat = plt.matshow(corr, fignum = 1)
+    corrMat = plt.matshow(corr, fignum=1)
     plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
     plt.yticks(range(len(corr.columns)), corr.columns)
     plt.gca().xaxis.tick_bottom()
@@ -58,24 +57,24 @@ def plotCorrelationMatrix(df, graphWidth):
 
 # scatter and density plots
 def plotScatterMatrix(df, plotSize, textSize):
-    df = df.select_dtypes(include =[np.number]) # keep only numerical columns
+    df = df.select_dtypes(include=[np.number])  # keep only numerical columns
     # Remove rows and columns that would lead to df being singular
     df = df.dropna('columns')
-    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    df = df[[col for col in df if df[col].nunique() > 1]]  # keep columns where there are more than 1 unique values
     columnNames = list(df)
-    if len(columnNames) > 10: # reduce the number of columns for matrix inversion of kernel density plots
+    if len(columnNames) > 10:  # reduce the number of columns for matrix inversion of kernel density plots
         columnNames = columnNames[:10]
     df = df[columnNames]
     ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
     corrs = df.corr().values
-    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
+    for i, j in zip(*plt.np.triu_indices_from(ax, k=1)):
         ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction',
                           ha='center', va='center', size=textSize)
     plt.suptitle('Scatter and Density Plot')
     plt.show()
 
 
-nRowsRead = 1000 # specify 'None' if want to read whole file
+nRowsRead = 1000  # specify 'None' if want to read whole file
 # mpii_human_pose.csv may have more rows in reality, but we are only loading/previewing the first 1000 rows
 # read data
 data = pd.read_csv('../data/mpii_human_pose.csv', delimiter=',', nrows=nRowsRead)
