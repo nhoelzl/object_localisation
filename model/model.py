2e24e2a58e7ecfaa5a2396fd2d69b83a0df571bb
Nicole Hoelzl <hoelzl.nici@gmail.com>
2021-01-08 10:59:07 +0100

    research trainable models

--- model/model.py
+++ model/model.py
@@ -8,3 +8,7 @@ Created on 30.12.2020, by Nicole Hölzl
     # Search for trainable models in Tensorflow
     # Fix tensorflow-hub issue
 
+    # https://www.tensorflow.org/hub/tutorials/object_detection
+    # https://www.analyticsvidhya.com/blog/2020/08/top-4-pre-trained-models-for-image-classification-with-python-code/
+
+    
