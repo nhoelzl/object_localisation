fbc9f6053c8e3401d670cff6c541a57e9dbf98ed
Micha Birklbauer <t0xic-m@users.noreply.github.com>
2021-01-28 01:53:01 +0100

    update annotator.sh

--- data/annotator.sh
+++ data/annotator.sh
@@ -1,3 +1,11 @@
-pip install --upgrade pip
-pip install labelImg
+#!/bin/bash
+
+## upgrade pip if necessary
+# pip install --upgrade pip
+
+## install labelImg if not installed
+## -> https://github.com/tzutalin/labelImg
+# pip install labelImg
+
+## run labelImg
 labelImg
