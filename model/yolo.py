2a01b5aab4aff4d180ab7b0e4931370627550422
Nicole Hoelzl <hoelzl.nici@gmail.com>
2021-01-29 15:29:30 +0100

    update .gitignore and rename model.py to yolo.py

---
+++ model/yolo.py
@@ -0,0 +1,31 @@
+"""
+Created on 30.12.2020, by Nicole Hölzl
+"""
+
+# import packages
+import tensorflow as tf
+import tensorflow_hub as hub
+
+# TODO:
+    # Search for trainable models in Tensorflow
+    # Fix tensorflow-hub issue
+
+    # Object Detection:
+    # https://www.tensorflow.org/hub/tutorials/object_detection
+
+    # Trainable Classifier (without Localisation)
+    # Trainable Yolo Modell and Har - Cascade
+    # https://github.com/shahkaran76/yolo_v3-tensorflow-ipynb/blob/master/YOLO%20Tensorflow.ipynb
+    # Data Augmentation - Flip Image, Mean Substraction
+
+
+    # pick 10 images with one person on it for testing
+    
+
+
+
+
+
+
+
+
