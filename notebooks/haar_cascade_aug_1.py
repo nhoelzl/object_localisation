d98d7402a894744f29d63b6dff38112af0347fb4
Micha Birklbauer <t0xic-m@users.noreply.github.com>
2021-01-31 09:54:21 +0100

    add haar cascade with augmentation

---
+++ notebooks/haar_cascade_aug_1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# 2021 (c) Micha Johannes Birklbauer
+#
+# https://github.com/t0xic-m/
+# micha.birklbauer@gmail.com
+
+import numpy as np
+import cv2
+import os
+import subprocess
+import fileinput
+import time
+
+
+# path definitions
+pp = "D:\\Users\\Micha\\Documents\\GitHub\\object_localisation\\notebooks\\" # project_path
+trainImgPathPos = pp + "haar_cascade_aug\\refImages_raw"
+# false images taken from:
+# https://ai.vub.ac.be/
+# http://arti.vub.ac.be/research/colour/data/imagesets.zip
+trainImgPathNeg = pp + "haar_cascade_aug\\false_images"
+
+openCVbinPath = pp + "haar_cascade\\opencv\\build\\x64\\vc15\\bin"
+openCVAnnotationToolExe = "opencv_annotation.exe"
+openCVSamplesToolExe = "opencv_createsamples.exe"
+openCVTrainCascadeToolExe = "opencv_traincascade.exe"
+cascadeResultPath = pp + "haar_cascade_aug\\trained_cascade"
+
+tgtFileExtensionTuple = ext = [".JPEG", ".jpg", ".png"]
+fileDictName = "out.txt"
+annotationFileName = "annotation.txt"
+vectorFileName = "samples.vect"
+
+def prepareSamplesAsVectFile(annotationFilePath, bgImages, vectorFileName, doShow = False) :
+    prepareSamplesCommand = os.path.join(openCVbinPath, openCVSamplesToolExe)
+    vectorFile = os.path.join(annotationFilePath, vectorFileName)
+    annotationFile = os.path.join(annotationFilePath, annotationFileName)
+    numVal = 1953
+    widthVal = 32
+    heightVal = 64
+    bgThreshVal = 0
+    maxAngleVal = 0
+    prepareSamplesCommand = prepareSamplesCommand + " -vec " + str(vectorFile) + " -info " + \
+    str(annotationFile) + " -bg " + str(bgImages) + " -num " + str(numVal) + " -w " + str(widthVal) + " -h " + str(heightVal)
+    if doShow:
+        prepareSamplesCommand = prepareSamplesCommand + " -show"
+    print("trying to call command: " + prepareSamplesCommand)
+    os.system(prepareSamplesCommand)
+
+def trainHaarCascade(cascadeResultPath, annotationFilePath, vectorFileName, bgImages) :
+    vectorFile = os.path.join(annotationFilePath, vectorFileName)
+    prepareSamplesCommand = os.path.join(openCVbinPath, openCVTrainCascadeToolExe)
+    widthVal = 32
+    heightVal = 64
+    numStages = 20
+    numPositives = 15
+    numNegatives = 200
+    maxFalseAlarmRate = 0.6
+
+    prepareSamplesCommand = prepareSamplesCommand + " -data " + str(cascadeResultPath) + " -vec " + str(vectorFile) + " -bg " + \
+    str(bgImages) + " -w " + str(widthVal) + " -h " + str(heightVal) + " -numStages " + str(numStages) + " -numPos " + \
+    str(numPositives) + " -numNeg " +str(numNegatives) + " -maxFalseAlarmRate " + str(maxFalseAlarmRate)
+    print("trying to call command: " + prepareSamplesCommand)
+    os.system("start /wait cmd /c " + str(prepareSamplesCommand))
+    input("Press Enter to continue...")
+
+prepareSamplesAsVectFile(trainImgPathPos, os.path.join(trainImgPathNeg, fileDictName), vectorFileName)
